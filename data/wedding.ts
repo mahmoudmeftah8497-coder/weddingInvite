d41a9eedebcd050dc741d6f534f23861271e78e4
mahmoud meftah <mahmoud.meftah@tu-dortmund.de>
2026-07-31 22:17:34 +0200

    Update location

--- data/wedding.ts
+++ data/wedding.ts
@@ -25,9 +25,9 @@ export const wedding = {
       icon: "💍",
       date: "31 October 2026",
       time: "4:00 PM",
-      venue: "Municipalité La Marsa",
+      venue: "Ksar Essaada Palace La Marsa",
       city: "La Marsa",
-      map: "https://maps.google.com/?q=Municipalité+La+Marsa",
+      map: "https://www.google.com/maps/place/Ksar+Essaada+Palace/@36.8816338,10.3263918,16.84z/data=!4m9!1m2!2m1!1sMunicipalit%C3%A9+La+Marsa!3m5!1s0x12e2b546b25f3577:0xb92c924f38cc1b!8m2!3d36.8802801!4d10.329833!16s%2Fg%2F11h6mnncrr?entry=ttu&g_ep=EgoyMDI2MDcyOS4wIKXMDSoASAFQAw%3D%3D",
     },
   ],
 };
